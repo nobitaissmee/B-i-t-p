ed78e98db4a59181f12a47c0a6364b0fd6725a1a
nobitaissmee <futhinh14101@gmail.com>
2020-10-05 20:42:09 +0700

    push

---
+++ Bai-tap/Biến,kiểu dữ liệu,toán tử/dientich.js
@@ -0,0 +1,4 @@
+let chieurong = Number(prompt("Nhap chieu rong", ""));
+let chieucao = Number(prompt("Nhap chieu cao", ""));
+let dientich = chieurong * chieucao;
+document.write("Dien tich hcn:" + dientich);
